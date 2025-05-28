351b7d82e9a787ac77bbfbaeafc70e6336c94768
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-05-28 10:52:31 +0300

    added urls and createadmin command

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        ordering = [
+            "email",
+        ]
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from users.apps import UsersConfig
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+    path("login/", UserTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
