61e801764755b0d7b783dc2063b9b661a9ab1de2
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-05-29 10:48:28 +0300

    urls for api

--- api/urls.py
+++ api/urls.py
@@ -11,6 +11,17 @@ urlpatterns = [
     path("cell/create/", views.SalesNetworkCellCreateView.as_view(), name="cell-create"),
     path("cell/<int:pk>/update/", views.SalesNetworkCellUpdateView.as_view(), name="cell-update"),
     path("cell/<int:pk>/delete", views.SalesNetworkCellDestroyView.as_view(), name="cell-destroy"),
-    path("contacts/", views.Co)
+
+    path("contacts/", views.ContactListView.as_view(), name="contacts-list" ),
+    path("contact/<int:pk>/", views.ContactDetailView.as_view(), name="contact-detail"),
+    path("contact/create/", views.ContactCreateView.as_view(), name="contact-create"),
+    path("contact/<int:pk>/update/", views.ContactUpdateView.as_view(), name="contact-update"),
+    path("contact/<int:pk>/delete", views.ContactDestroyView.as_view(), name="contact-destroy"),
+
+    path("products/", views.ProductListView.as_view(), name="products-list"),
+    path("product/<int:pk>/", views.ProductDetailView.as_view(), name="product-detail"),
+    path("product/create/", views.ProductCreateView.as_view(), name="product-create"),
+    path("product/<int:pk>/update/", views.ProductUpdateView.as_view(), name="product-update"),
+    path("product/<int:pk>/delete", views.ProductDestroyView.as_view(), name="product-destroy"),
 
 ]
